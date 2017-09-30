c7653f85ab06182cc95c1a64551f317b30788080
farir1408 <farir1881@gmail.com>
2017-09-30 20:01:55 +0300

    Create M1_1-6.cpp

---
+++ M1_1-6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//Разворачивание массива
+using namespace std;
+
+void reverse(int *a, int n){
+    for(int i = 0; i < n / 2; i++){
+        int tmp = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = tmp;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    reverse(a, n);
+    for(int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    return 0;
+}
