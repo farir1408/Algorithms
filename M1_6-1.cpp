e572865599fa7a1d44c99a63646cb1f2b693177c
farir1408 <farir1881@gmail.com>
2017-10-03 02:08:36 +0300

    Create M1_6-1.cpp

---
+++ M1_6-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+//high pyramid
+using namespace std;
+
+int f_piramids(int **table, int blocks) {
+    int result = 0;
+    for (int k = 1; k <= blocks; k++) {
+        for (int m = 1; m <= blocks; m++) {
+            if (k - m == 0) {
+                table[k][m] = 1;
+            }
+            else {
+                table[k][m] = 0;
+                for (int r = 1; r <= m; r++) {
+                    if (k - m > 0)
+                        table[k][m] += table[k - m][r];
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= blocks; i++)
+        result += table[blocks][i];
+    return result;
+}
+
+int main() {
+    int n = 0;
+    cin >> n;
+
+    int **table = new int*[n + 1];
+    for (int i = 0; i <= n; i++)
+        table[i] = new int[n + 1];
+
+    cout << f_piramids(table, n);
+
+    for (int i = 0; i <= n; i++)
+        delete[] table[i];
+
+}
