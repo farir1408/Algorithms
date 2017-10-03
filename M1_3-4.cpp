28d140cbe8acfa835b952f2c641a285dd5faecbd
farir1408 <farir1881@gmail.com>
2017-10-03 17:34:52 +0300

    Update M1_3-4.cpp

--- M1_3-4.cpp
+++ M1_3-4.cpp
@@ -2,51 +2,43 @@
 #include <cmath>
 
 using namespace std;
-//Нахождение ближайшего значения
 
-int find(int *a, int key, int n){
-    int left = 0;
-    int right = n - 1;
+int BinarySearch(int* arr, int left, int right,
+                 int value, bool direction) {
+    int mid = left + (right - left) / 2;
     
-    while (true){
-        int mid = left + (right - left) / 2;
-        if (a[mid] == key)
-            return mid;
-
-        if ((a[mid] < key && key < a[mid + 1]) && mid != n - 1)
-            return (abs(key - a[mid]) < abs(a[mid + 1] - key + 1)) ? mid : mid + 1;
-        else {
-            if (mid == n - 1)
-                return mid;
-            if (mid == 0)
-                return 0;
-        }
-
-        if (a[mid] > key)
-            right = mid;
-        else
-            left = mid + 1;
-    }
+    if (right - left == 1)
+        return value <= arr[left] ? left : right - 1;
+    else if((value < arr[mid]) ^ direction)
+        return BinarySearch(arr, left, mid, value, direction);
+    else
+        return BinarySearch(arr, mid, right, value, direction);
 }
 
-int main(){
-    int m, n;
+int FindInsert(int* arr, int count, int value) {
+    if (count == 0)
+        return -1;
+    bool direction = arr[0] > arr[count-1];
+    int point = BinarySearch(arr, 0, count, value, direction);
+    return (abs(value - arr[point]) < abs(arr[point + 1] - value + 1)) ? point : point + 1;
+}
+
+int main() {
+    int n = 0;
     cin >> n;
-    int *a = new int[n];
+    int* a = new int[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
+    int m = 0;
     cin >> m;
-    int *b = new int[m];
-//    bool direction = a[0] > a[n-1];
+    int* b = new int[m];
     for (int i = 0; i < m; i++)
         cin >> b[i];
 
-    for (int i = 0; i < m; i++){
-        int key = b[i];
-        cout << find(a, key, n) << " ";
-    };
-
+    for (int i = 0; i < m; i++) {
+        int value = b[i];
+        cout << FindInsert(a, n, value) << " ";
+    }
     delete[] a;
     delete[] b;
 }
-
