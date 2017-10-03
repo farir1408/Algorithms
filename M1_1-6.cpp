8f5cc764c23a0dd4b052a93d9ece5bf50d7344dc
farir1408 <farir1881@gmail.com>
2017-10-03 15:33:30 +0300

    Update M1_1-6.cpp

--- M1_1-6.cpp
+++ M1_1-6.cpp
@@ -3,23 +3,22 @@
 //Разворачивание массива
 using namespace std;
 
-void reverse(int *a, int n){
-    for(int i = 0; i < n / 2; i++){
+void reverse(int *a, int n) {
+    for (int i = 0; i < n / 2; i++) {
         int tmp = a[i];
         a[i] = a[n-i-1];
         a[n-i-1] = tmp;
     }
 }
 
-int main()
-{
-    int n;
+int main() {
+    int n = 0;
     cin >> n;
     int a[n];
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cin >> a[i];
     reverse(a, n);
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cout << a[i] << " ";
     return 0;
 }
