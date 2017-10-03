52dcb7db7f90db8e91694070700adbf8b22cdce9
farir1408 <farir1881@gmail.com>
2017-10-03 15:37:26 +0300

    Update M1_2-4.cpp

--- M1_2-4.cpp
+++ M1_2-4.cpp
@@ -3,13 +3,13 @@
 //Считалка Джозефуса
 using namespace std;
 
-int josephus(int n, int k){
+int josephus(int n, int k) {
     return n > 1 ? (josephus(n - 1, k) + k - 1) % n + 1 : 1;
 }
 
-int main()
-{
-    int n, k;
+int main() {
+    int n = 0;
+    int k = 0;
     cin >> n >> k;
     cout << josephus(n, k) << "\n";
     return 0;
