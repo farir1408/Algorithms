80e96d14eff82d2558e87310a811451bc2566519
farir1408 <farir1881@gmail.com>
2017-10-10 01:53:41 +0300

    Update M1_1-6.cpp

--- M1_1-6.cpp
+++ M1_1-6.cpp
@@ -15,9 +15,12 @@ int main() {
     int n = 0;
     cin >> n;
     int a[n];
+    
     for (int i = 0; i < n; i++)
         cin >> a[i];
+    
     reverse(a, n);
+    
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
     return 0;
