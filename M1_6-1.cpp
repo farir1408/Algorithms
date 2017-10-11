c32b38b9818af662b38936fc66936161d9a711b3
farir1408 <farir1881@gmail.com>
2017-10-11 16:40:01 +0300

    Update M1_6-1.cpp

--- M1_6-1.cpp
+++ M1_6-1.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int f_piramids(int **table, int blocks) {
     int result = 0; // k - number of cubes, m - number of cubes at the bottom of pyramid
     for (int k = 1; k <= blocks; k++) {
+        
         for (int m = 1; m <= blocks; m++) {
             if (k - m == 0) {
                 table[k][m] = 1;
-            }
-            else {
+            } else {
                 table[k][m] = 0;
+                
                 for (int r = 1; r <= m; r++) {
                     if (k - m > 0)
                         table[k][m] += table[k - m][r];
@@ -19,8 +20,10 @@ int f_piramids(int **table, int blocks) {
             }
         }
     }
+    
     for (int i = 1; i <= blocks; i++)
         result += table[blocks][i];
+    
     return result;
 }
 
@@ -36,5 +39,6 @@ int main() {
 
     for (int i = 0; i <= n; i++)
         delete[] table[i];
-
+    
+    delit[] table;
 }
