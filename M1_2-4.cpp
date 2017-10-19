d2015f9ec93316ec7cc1020ba73214f02456b55a
Oleg Venger <farir1881@gmail.com>
2017-10-19 21:25:49 +0300

    Update M1_2-4.cpp

--- M1_2-4.cpp
+++ M1_2-4.cpp
@@ -7,6 +7,13 @@ int josephus(int n, int k) {
     return n > 1 ? (josephus(n - 1, k) + k - 1) % n + 1 : 1;
 }
 
+//int josephus(int n, int k) {
+//    int res = 1;
+//    for (int i = 2; i <= n; ++i)
+//        res = (res + k - 1) % i + 1;
+//    return res;
+//}
+
 int main() {
     int n = 0;
     int k = 0;
