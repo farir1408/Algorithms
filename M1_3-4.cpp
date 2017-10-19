bd46470ead7c5d663f209bcc2dac3492894c48dc
Oleg Venger <farir1881@gmail.com>
2017-10-19 21:28:20 +0300

    Update M1_3-4.cpp

--- M1_3-4.cpp
+++ M1_3-4.cpp
@@ -19,19 +19,37 @@ int FindInsert(int* arr, int count, int value) {
     if (count == 0)
         return -1;
     bool direction = arr[0] > arr[count-1];
-    int point = BinarySearch(arr, 0, count, value, direction);
-    return (abs(value - arr[point]) < abs(arr[point + 1] - value + 1)) ? point : point + 1;
+    
+    if (value > arr[count - 1])
+       return count - 1;
+    int prev = 0;
+    int next = 1;
+       
+    while (true) {
+        if (value <= arr[next]) {
+            int point = BinarySearch(arr, prev, next, value, direction);
+            return (abs(value - arr[point]) < abs(arr[point + 1] - value + 1)) ? point : point + 1;
+        }
+        if (next * 2 < count) {
+            prev = next;
+            next = next * 2;
+        } else {
+            next = count - 1;
+        }
+    }
 }
 
 int main() {
     int n = 0;
     cin >> n;
     int* a = new int[n];
+  
     for (int i = 0; i < n; i++)
         cin >> a[i];
     int m = 0;
     cin >> m;
     int* b = new int[m];
+  
     for (int i = 0; i < m; i++)
         cin >> b[i];
 
@@ -39,6 +57,7 @@ int main() {
         int value = b[i];
         cout << FindInsert(a, n, value) << " ";
     }
+  
     delete[] a;
     delete[] b;
 }
